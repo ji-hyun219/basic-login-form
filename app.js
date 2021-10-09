b8c9bab8c6a31efc83e9e46eba6b18af7a708317
ji-hyun219 <91349474+ji-hyun219@users.noreply.github.com>
2021-10-10 06:18:29 +0900

    Keep remembering username
    its previous version is not remembering username.
    F5 -> login form is present.
    
    But now, this version is keep remembering username.
    So... F5 -> "Hello username"

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const loginForm = document.querySelector('#login_form');
 const loginInput = document.querySelector('#login_form input');
 const greeting = document.querySelector('#greeting');
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = 'username';
+
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    localStorage.setItem('username', username);
+    localStorage.setItem(USERNAME_KEY, username);  / remembered username on DB /
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `hello ${username}`;
+    paintGreetings(username);
     
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `hello ${username}`;
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the login form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+}
+else {
+    // show the greeting
+    paintGreetings(savedUsername);
+}
